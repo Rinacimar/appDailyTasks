748a9edaa9407d55d52b04f69e53082132483d4f
Rinacimar <Rinacimar@users.noreply.github.com>
2020-12-10 09:51:39 -0300

    Correções

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'tela-inicial',
     pathMatch: 'full'
   },
   {
-    path: 'clima',
-    loadChildren: () => import('./clima/clima.module').then( m => m.ClimaPageModule)
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
   {
     path: 'clima',
     loadChildren: () => import('./clima/clima.module').then( m => m.ClimaPageModule)
   },
-  {
+    {
     path: 'lista-de-compras',
     loadChildren: () => import('./lista-de-compras/lista-de-compras.module').then( m => m.ListaDeComprasPageModule)
   },
